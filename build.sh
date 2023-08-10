17dfdd6de837d324323108939198f85927e5b543
silenceoverflow <21084990+SilenceOverflow@users.noreply.github.com>
2023-08-10 14:09:32 -0700

    disable compilation warnings

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
+clear && clear
+
 echo "Configuring and building Thirdparty/DBoW2 ..."
 
 cd Thirdparty/DBoW2
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j48
 
 cd ../../g2o
 
@@ -13,7 +15,7 @@ echo "Configuring and building Thirdparty/g2o ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j48
 
 cd ../../Sophus
 
@@ -22,7 +24,7 @@ echo "Configuring and building Thirdparty/Sophus ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j48
 
 cd ../../../
 
@@ -37,4 +39,4 @@ echo "Configuring and building ORB_SLAM3 ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j4
+make -j48
