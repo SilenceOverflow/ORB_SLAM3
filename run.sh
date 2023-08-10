2ac716036e569b90dc3dae9326dd43c9d64f519a
silenceoverflow <21084990+SilenceOverflow@users.noreply.github.com>
2023-08-10 14:09:32 -0700

    fix compilaiton on Ubuntu 20.04

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 clear && clear
 
-./Examples/Monocular/mono_euroc Vocabulary/ORBvoc.txt Examples/Monocular/EuRoC.yaml ~/EuRoCDataset/MH_01_easy Examples/Monocular/EuRoC_TimeStamps/MH01.txt 
+# ./Examples/Monocular/mono_euroc Vocabulary/ORBvoc.txt Examples/Monocular/EuRoC.yaml ~/EuRoCDataset/MH_01_easy Examples/Monocular/EuRoC_TimeStamps/MH01.txt 
+
+./Examples/Stereo-Inertial/stereo_inertial_euroc Vocabulary/ORBvoc.txt Examples/Stereo-Inertial/EuRoC.yaml ~/EuRoCDataset/V1_01_easy Examples/Stereo-Inertial/EuRoC_TimeStamps/V101.txt 
