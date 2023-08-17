1b82e9838d544cba0ab9fe3ca7a4748a24ec0ce8
SilenceOverflow <21084990+SilenceOverflow@users.noreply.github.com>
2023-08-17 15:28:17 +0800

    fix build ros

--- build_ros.sh
+++ build_ros.sh
@@ -2,8 +2,10 @@ clear && clear
 
 echo "Building ROS nodes"
 
+export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:${PWD}/Examples_old/ROS
+
 cd Examples_old/ROS/ORB_SLAM3
 mkdir build
 cd build
 cmake .. -DROS_BUILD_TYPE=Release
-make -j48
+make -j2
